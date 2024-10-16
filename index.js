ac043e9f00821916d8c518a020bdb1ec8d8f6a5d
jobwayne <otienojobwayneotieno@gmail.com>
2024-10-16 21:22:35 +0300

    objects

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+const employee = {
+    name: "Wayne",
+    streetAddress: "13 Manhattan"
+};
+ 
+function updateEmployeeWithKeyAndValue(employee , key, value){
+        const newemployee = {...employee, [key]:value};
+        return newemployee;       
+}
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
+    employee[key]=value;
+    return employee;   
+}
+function deleteFromEmployeeByKey(employee,key) {
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;   
+} 
+function destructivelyDeleteFromEmployeeByKey (employee,key) {
+    delete employee[key];
+    return employee;
+}
+
+
+
+
+
+
+
+
+   
+
+
